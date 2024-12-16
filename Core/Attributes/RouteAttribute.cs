b34e3b733fcfa2675307a42fc1c3569804e6da3a
Mestu Paul <paulmestu@gmail.com>
2024-12-16 21:09:11 +0600

    add core project

---
+++ Core/Attributes/RouteAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace Core.Attributes
+{
+    public class RouteMethodAttribute : Attribute
+    {
+        public string RouteName { get; }
+
+        public RouteMethodAttribute(string routeName)
+        {
+            RouteName = routeName;
+        }   
+    }
+
+    public static class RouterMethodProvider
+    {
+        public static Dictionary<string, MethodInfo> GetRoutedMethods(Type assemblyType)
+        {
+            var methods =  assemblyType.GetMethods()
+                .Where(m => m.GetCustomAttribute<RouteMethodAttribute>() != null)
+                .ToDictionary(
+                    x => x.GetCustomAttribute<RouteMethodAttribute>()!.RouteName,
+                    x => x
+                );
+            return methods;
+        }
+    }
+}
